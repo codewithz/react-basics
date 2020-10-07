37f0e35fbabb322e1147bbae444864c83e26f985
Zartab Nakhwa <nzartab@gmail.com>
2020-10-07 17:04:22 +0530

    Redirection on Condition

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,49 @@
 import React,{Component} from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 
 
 class About extends Component
 {
+    constructor()
+    {
+        super();
+        this.state={
+            loginStatus:false
+        }
+    }
+    changeLogin()
+    {
+        this.setState(
+            {
+            loginStatus:!this.state.loginStatus
+            }
+        )
+       
+    }
     render()
     {
+        if(this.state.loginStatus)
+        {
+            return  <Redirect to="/signup">
+           
+       </Redirect> 
+        }
+        else
+        {
         return(
             <div>
                 <p>This is About us Page!!</p>
                 <Button component={Link} to="/login" variant="outlined">
                Go to Login Page
                 </Button>
+
+                <Button onClick={this.changeLogin.bind(this)} variant="outlined">
+               Change Login Status
+                </Button>
             </div>
         )
+        }
     }
 
 }
